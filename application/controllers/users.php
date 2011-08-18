ab75c676733adbbba65179d174ad1060c4caa561
Larry Kubin <larry.kubin@gmail.com>
2011-08-18 17:48:51 -0500

    fixed formatting caused by new editor

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,13 +1,13 @@
 <?
 class Users extends CI_Controller
 {
-	public function logout()
-	{
-		$fb_app_id = $this->config->item('fb_app_id');
-		unset($_SESSION['fb_' . $fb_app_id . '_user_id']);
-		unset($_SESSION['fb_' . $fb_app_id . '_code']);
-		unset($_SESSION['fb_' . $fb_app_id . '_state']);
-		$this->session->sess_destroy();
-		redirect(base_url());
-	}
+    public function logout()
+    {
+        $fb_app_id = $this->config->item('fb_app_id');
+        unset($_SESSION['fb_' . $fb_app_id . '_user_id']);
+        unset($_SESSION['fb_' . $fb_app_id . '_code']);
+        unset($_SESSION['fb_' . $fb_app_id . '_state']);
+        $this->session->sess_destroy();
+        redirect(base_url());
+    }
 }
--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,17 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
-	public $require_fb = array('secret');
+class Welcome extends CI_Controller 
+{
+    public $require_fb = array('secret');
 
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    public function index()
+    {
+        $this->load->view('welcome_message');
+    }
 
-	public function secret()
-	{
-		$this->load->view('secret_message');
-	}
+    public function secret()
+    {
+        $this->load->view('secret_message');
+    }
 }
 
 /* End of file welcome.php */
